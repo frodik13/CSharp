eab324d436c562cd709b950a6b753319a927f5d3
frodik13 <103507712+frodik13@users.noreply.github.com>
2022-07-12 10:56:13 +0400

    Счастливый билет
    Определить, считается ли введенный номер билета счастливым или нет

---
+++ HappyTicket/HappyTicket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер билета: ");
+            int ticket = int.Parse(Console.ReadLine());
+
+            int part1 = ticket / 1000;
+            int part2 = ticket % 1000;
+
+            int a = 0;
+            while (part1 > 0) {
+                a += part1 % 10;
+                part1 /= 10;
+            }
+
+            int b = 0;
+            while (part2 > 0) { 
+                b += part2 % 10; 
+                part2 /= 10;
+            }
+
+            if (a == b)
+            {
+                Console.WriteLine("Поздравляю, Ваш билет счастливый!");
+            }
+            else
+            {
+                Console.WriteLine("Немножко не повезло :(");
+            }
+        }
+    }
+}
