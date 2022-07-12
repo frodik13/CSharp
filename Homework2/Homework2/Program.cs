61c581b92becd65772b34a94c3c07e9ece137cee
frodik13 <103507712+frodik13@users.noreply.github.com>
2022-07-12 11:13:07 +0400

    Домашнее задание 2
    Работа светофора для пешеходов запрограммирована следующим образом: в начале каждого часа в течение трех минут горит зеленый сигнал, затем в течение двух минут – красный, в течение трех минут – опять зеленый и т. д. Дано вещественное число t, означающее время в минутах, прошедшее с начала очередного часа. Определить, сигнал какого цвета горит для пешеходов в этот момент.

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число минут, прошедних с начала часа: ");
+            var minute = double.Parse(Console.ReadLine());
+
+            while (minute >= 0)
+            {
+                minute -= 3;
+                if (minute < 0)
+                {
+                    Console.WriteLine("Горит зеленый сигнал светофора");
+                    break;
+                }
+                minute -= 2;
+                if (minute < 0)
+                {
+                    Console.WriteLine("Горит красный сигнал светофора");
+                    break;
+                }
+            }
+        }
+    }
+}
