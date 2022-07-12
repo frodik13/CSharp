e551b78bb108e5de30029dc3ddb6a2e5943ec299
frodik13 <103507712+frodik13@users.noreply.github.com>
2022-07-12 10:56:56 +0400

    Домашнее задание 1
    1) С начала суток прошло n секунд. Определить:
    а) сколько полных часов прошло с начала суток;
    б) сколько полных минут прошло с начала очередного часа;
    в) сколько полных секунд прошло с начала очередной минуты.

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество секунд: ");
+            var sec = int.Parse(Console.ReadLine());
+
+            int hour = sec / 3600;
+            int minute = (sec - hour * 3600) / 60;
+            int second = sec - hour * 3600 - minute * 60;
+
+
+
+            Console.WriteLine("{0}:{1}:{2}", hour, minute, second);
+        }
+    }
+}
